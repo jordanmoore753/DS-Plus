f30d0450536de10789544c1c90e75bca58f9855c
jordanmoore753 <jordanmoore753@gmail.com>
2020-02-26 08:48:44 -0500

    tests for graph 80%

---
+++ test/adjacencylist.test.js
@@ -0,0 +1,326 @@
+const Plus = require('../app.js');
+
+describe('Graph', () => {
+  // add option for two or one way connected
+  it('should be initialized with a string', () => {
+    expect(() => {
+      new Plus.Graph();
+    }).toThrow('Data type must be: string, number, object.');
+
+    expect(() => {
+      new Plus.Graph('random');
+    }).toThrow('Data type must be: string, number, object.');
+
+    let g = new Plus.Graph('string');
+    expect(g).toEqual({
+      data: {},
+      type: 'string',
+      size: 0
+    });
+
+    g = new Plus.Graph('number');
+    expect(g).toEqual({
+      data: {},
+      type: 'number',
+      size: 0
+    });
+
+    g = new Plus.Graph('object');
+    expect(g).toEqual({
+      data: {},
+      type: 'object',
+      size: 0
+    });
+  });
+
+  it('should add keys as values to data', () => {
+    // number
+
+    let g = new Plus.Graph('number');
+
+    expect(g.addVertices(1)).toBe(g);
+    expect(g.data['1']).toEqual([]);
+
+    expect(() => {
+      g.addVertices(1);
+    }).toThrow('Vertice already exists in graph.');
+
+    g.addVertices(2, 3, 4);
+    expect(g.data['2']).toEqual([]);
+    expect(g.data['3']).toEqual([]);
+    expect(g.data['4']).toEqual([]);
+
+    // object
+
+    g = new Plus.Graph('object');
+
+    g.addVertices({ id: 1 });
+    expect(g.data[{ id: 1 }.toString()]).toEqual([]);
+
+    expect(() => {
+      g.addVertices({ id: 1 });
+    }).toThrow('Vertice already exists in graph.');
+
+    g.addVertices({ id: 2 }, { id: 3 });
+    expect(g.data[{ id: 2 }.toString()]).toEqual([]);
+    expect(g.data[{ id: 3 }.toString()]).toEqual([]); 
+
+    // string
+
+    g = new Plus.Graph('string');
+
+    g.addVertices('first');
+    expect(g.data.first).toEqual([]);
+
+    g.addVertices('second', 'third');
+    expect(g.data.second).toEqual([]);
+    expect(g.data.third).toEqual([]);
+
+    // no data passed
+
+    expect(g.addVertices()).toBe(g); 
+  });
+
+  it('should throw error for adding the wrong type of data', () => {
+    // string
+
+    let g = new Plus.Graph('string');
+
+    expect(() => {
+      g.addVertices(1);
+    }).toThrow('Incorrect data type for this graph.');
+
+    expect(() => {
+      g.addVertices('first', 'second', { id: 1 });
+    }).toThrow('Incorrect data type for this graph.');
+
+    // number
+
+    g = new Plus.Graph('number');
+
+    expect(() => {
+      g.addVertices({ id: 1 });
+    }).toThrow('Incorrect data type for this graph.');
+
+
+    expect(() => {
+      g.addVertices(1, 'string');
+    }).toThrow('Incorrect data type for this graph.');
+
+    // object
+
+    g = new Plus.Graph('object');
+
+    expect(() => {
+      g.addVertices('string');
+    }).toThrow('Incorrect data type for this graph.');
+
+
+    expect(() => {
+      g.addVertices({ id: 1 }, 1);
+    }).toThrow('Incorrect data type for this graph.');
+  });
+
+  it('should link keys to specific values in array', () => {
+    // number
+
+    let g = new Plus.Graph('number');
+    g.addVertices(2, 3, 4, 5, 6, 7, 8);
+
+    expect(() => {
+      g.addEdges(2, [1]);
+    }).toThrow('Value in edges array not a valid vertex.');
+
+    expect(() => {
+      g.addEdges(2, 3, 4, 5);
+    }).toThrow('Edges must be in array.');
+
+    expect(() => {
+      g.addEdges(2, [2]);
+    }).toThrow('Value in edges array cannot be the first argument.');
+
+    expect(g.addEdges(2, [3, 4, 5])).toBe(g);
+    expect(g.data[2]).toEqual([3, 4, 5]);
+
+    // check that ALL edges were added to relevant vertices
+    expect(g.data[3]).toEqual([2]);
+    expect(g.data[4]).toEqual([2]);
+    expect(g.data[5]).toEqual([2]);
+
+    expect(() => {
+      g.addEdges(2, [3]);
+    }).toThrow('Vertex already exists as connection for passed vertex.');
+
+    // object
+
+    g = new Plus.Graph('object');
+    g.addVertices({ id: 1 });
+    g.addVertices({ id: 2 }, { id: 3 }, { id: 4 }, { id: 5 });
+
+    expect(() => {
+      g.addEdges({ id: 1 }, [{ id: 0 }]);
+    }).toThrow('Value in edges array not a valid vertex.');
+
+    expect(() => {
+      g.addEdges({ id: 1 }, [{ id: 1 }]);
+    }).toThrow('Value in edges array cannot be the first argument.');
+
+    expect(g.addEdges({ id: 1 }, [{ id: 2 }, { id: 3 }, { id: 4 }])).toBe(g);
+    expect(g.data[{ id : 1 }]).toEqual([{ id: 2 }, { id: 3 }, { id: 4 }]);
+
+    expect(() => {
+      g.addEdges({ id: 1 }, [{ id: 2 }]);
+    }).toThrow('Vertex already exists as connection for passed vertex.');
+    
+    // string
+
+    g = new Plus.Graph('string');
+    g.addVertices('first');
+    g.addVertices('second', 'third', 'four', 'fifth');
+
+    expect(() => {
+      g.addEdges('first', ['k']);
+    }).toThrow('Value in edges array not a valid vertex.');
+
+    expect(() => {
+      g.addEdges('first', ['first']);
+    }).toThrow('Value in edges array cannot be the first argument.');
+
+    expect(g.addEdges('first', ['third', 'four'])).toBe(g);
+    expect(g.data.first).toEqual(['third', 'four']);
+
+    expect(() => {
+      g.addEdges('first', ['third']);
+    }).toThrow('Vertex already exists as connection for passed vertex.');
+  });
+
+  it('should unlink keys with specific values in array', () => {
+    // number
+
+    let g = new Plus.Graph('number');
+    g.addVertices(2, 3, 4, 5, 6, 7, 8);
+    g.addEdges(2, [3, 4, 5]);
+
+    g.removeEdges(2, [3, 4, 5, 6]);
+    expect(g.data['2']).toEqual([]);
+
+    // object
+
+    g = new Plus.Graph('object');
+    g.addVertices({ id: 1 });
+    g.addVertices({ id: 2 }, { id: 3 }, { id: 4 }, { id: 5 });
+    g.addEdges({ id: 1 }, [{ id: 2 }, { id: 3 }, { id: 4 }]);
+
+    g.removeEdges({ id: 1 }, [{ id: 2 }]);
+    g.removeEdges({ id: 1 }, [{ id: 3 }, { id: 4 }]);
+    expect(g.data[{ id: 1 }.toString()]).toEqual([]);
+
+    // string
+
+    g = new Plus.Graph('string');
+    g.addVertices('first');
+    g.addVertices('second', 'third', 'four', 'fifth');
+    g.addEdges('first', ['third', 'four']);
+
+    expect(() => { 
+      g.removeEdges();
+    }).toThrow('Must have two arguments: vertex and edges array.');
+
+    expect(() => { 
+      g.removeEdges('first');
+    }).toThrow('Must have two arguments: vertex and edges array.');
+
+    expect(() => { 
+      g.removeEdges('first', 'one');
+    }).toThrow('Must have two arguments: vertex and edges array.');
+
+    expect(g.removeEdges('first', ['second']).toBe(g);
+    g.removeEdges('first', ['third']);
+    expect(g.data.first).toEqual(['four']);
+  });
+
+  it('should get all edges for specific key', () => {
+    // number
+
+    let g = new Plus.Graph('number');
+    g.addVertices(2, 3, 4, 5, 6, 7, 8);
+    g.addEdges(2, [3, 4, 5]);
+
+    expect(g.getEdges(2)).toEqual([3, 4, 5]);
+    expect(g.getEdges(3)).toEqual([]);
+    expect(() => {
+      g.getEdges(1);
+    }).toThrow('Vertex does not exist.');
+
+    // object
+
+    g = new Plus.Graph('object');
+    g.addVertices({ id: 1 });
+    g.addVertices({ id: 2 }, { id: 3 }, { id: 4 }, { id: 5 });
+    g.addEdges({ id: 1 }, [{ id: 2 }, { id: 3 }, { id: 4 }]);
+
+    expect(g.getEdges({ id: 1 })).toEqual([{ id: 2 }, { id: 3 }, { id: 4 }]);
+    expect(g.getEdges({ id: 2 })).toEqual([]);
+    expect(() => {
+      g.getEdges({ id: -1 });
+    }).toThrow('Vertex does not exist.'); 
+
+    // string
+
+    g = new Plus.Graph('string');
+    g.addVertices('first');
+    g.addVertices('second', 'third', 'four', 'fifth');
+    g.addEdges('first', ['third', 'four', 'fifth']);
+
+    expect(g.getEdges('first')).toEqual(['third', 'four', 'fifth']);
+    expect(g.getEdges('second')).toEqual([]);
+    expect(g.getEdges()).toEqual(null);
+    expect(() => {
+      g.getEdges('dd');
+    }).toThrow('Vertex does not exist.');
+  });
+
+  it('should return size of data', () => {
+    let g = new Plus.Graph('object');
+    expect(g.getSize()).toBe(0);
+
+    g.addVertices({ id: 1 });
+    g.addVertices({ id: 2 }, { id: 3 }, { id: 4 }, { id: 5 });
+    expect(g.getSize()).toBe(5);
+  });
+
+  it('should return number of edges for given value', () => {
+    let g = new Plus.Graph('object');
+    g.addVertices({ id: 1 });
+    g.addVertices({ id: 2 }, { id: 3 }, { id: 4 }, { id: 5 });
+    g.addEdges({ id: 1 }, [{ id: 2 }, { id: 3 }, { id: 4 }]);
+
+    expect(g.numberOfEdges({ id: 1 })).toBe(3);
+
+    g.removeEdges({ id: 1 }, [{ id: 2 }]);
+    expect(g.numberOfEdges({ id: 1 })).toBe(2);
+    expect(g.numberOfEdges({ id: 2 })).toBe(0);
+    expect(g.numberOfEdges({ id: -1 }).toBe(null); 
+  });
+
+  it('should return true || false if first param has second as edge', () => {
+    let g = new Plus.Graph('object');
+    g.addVertices({ id: 1 });
+    g.addVertices({ id: 2 }, { id: 3 }, { id: 4 }, { id: 5 });
+    g.addEdges({ id: 1 }, [{ id: 2 }, { id: 3 }, { id: 4 }]);
+
+    expect(g.hasEdge({ id: 1 }, { id: 2 })).toBe(true);
+    expect(g.hasEdge({ id: 1 }, { id: 5 })).toBe(false);
+    expect(g.hasEdge({ id: 2 }, { id: 1 })).toBe(true);
+    expect(() => { 
+      g.hasEdge({ id: 1 }, { id: -11 })
+    }).toThrow('Second vertex does not exist.');
+    expect(() => { 
+      g.hasEdge({ id: 111 }, { id: 2 })
+    }).toThrow('First vertex does not exist.');        
+  });
+
+  it('should remove vertex and remove all edges to it', () => {
+
+  });
+});
